e51e900937b2fc712745149f8023e4bfdf4f94d3
JamesChai123 <60818189+JamesChai123@users.noreply.github.com>
2024-05-04 10:33:14 +0800

    Add files via upload

--- game_manager.py
+++ game_manager.py
@@ -7,14 +7,20 @@ root.state('zoomed')
 Game_List = []
 
 pos = 0
+y_axis = 0
 
 def add_application():
     global pos
+    global y_axis
     Game_List.append("")
     if pos < len(Game_List):
         new_button = Button(root, text="Game {}".format(len(Game_List)))
-        new_button.place(x=125 + (375*(pos%4)),y=195)
+        new_button.place(x=125 + (375*(pos%4)),y=150 + (175*(y_axis)))
         pos = pos +1
+        print(y_axis)
+        print(pos%4)
+    if (pos%4) == 0:
+        y_axis += 1
 
 add = Button(text="Add Games", command=add_application)
 add.place(x=10, y=10)
