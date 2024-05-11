bdfe170cdd44ac2a6e8dbcc6428f8acb62e2e1e0
JamesChai123 <60818189+JamesChai123@users.noreply.github.com>
2024-05-11 10:17:20 +0800

    Add files via upload

--- game_manager.py
+++ game_manager.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from AppOpener import open
+from PIL import Image, ImageTk
 
 root = Tk()
 root.state('zoomed')
@@ -15,7 +17,7 @@ class Game_Manager:
     def __init__(self):
         self.entry = None 
 
-    def submit(self):
+    def submit(self,event=None):
         global game_name
         game_name = self.entry.get()
         self.add_application()
@@ -24,6 +26,7 @@ class Game_Manager:
     def game_input(self):
         global inputWindow
         inputWindow = Toplevel(root)
+        inputWindow.geometry("150x70")
         inputWindow.title("Add Application")
         name_label = tk.Label(inputWindow, text = "Game Name")
         name_label.pack()
@@ -31,14 +34,18 @@ class Game_Manager:
         self.entry.pack()
         sub_btn=tk.Button(inputWindow,text = 'Submit', command = self.submit)
         sub_btn.pack()
-
+        inputWindow.bind('<Return>', lambda event: self.submit())
+        
+    def open_game(self, game_name):
+        open(game_name, match_closest=True)
+                
     def add_application(self):
         global pos
         global y_axis
-        global game_name
+        global new_button
         Game_List.append(game_name)
         if pos < len(Game_List):
-            new_button = Button(root, text=game_name)
+            new_button = Button(root, text=game_name, command=lambda name=game_name: self.open_game(name))
             new_button.place(x=125 + (375*(pos%4)),y=150 + (175*(y_axis)))
             pos = pos +1
         if (pos%4) == 0:
